51a46e62431b794b0629788a71257c8aee226e6b
Dwarven YANG <prison.yang@gmail.com>
2015-09-16 15:35:48 +0800

    init project

---
+++ QRCode-Encoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode-Encoder
+//
+//  Created by Dwarven on 15/9/16.
+//  Copyright (c) 2015年 Dwarven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRCode-Encoder/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QRCode-Encoder
+//
+//  Created by Dwarven on 15/9/16.
+//  Copyright (c) 2015年 Dwarven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
